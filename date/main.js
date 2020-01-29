8a83bd95162e321619294194d606b0155f7cfaec
alekon94 <alekon43@gmail.com>
2020-01-29 21:22:23 +0300

    1
    1

--- date/main.js
+++ date/main.js
@@ -4,5 +4,14 @@ function getDate () {
     let now = Date.parse(String(date.getFullYear()) + '-' + (date.getMonth() + 1) + '-' + date.getDate());
     let birthday = Date.parse(val);
     let result = (birthday - now) / (1000 * 60 * 60 * 24);
-    document.getElementById("result").innerHTML = "До дня рождения осталось: " + Math.round(result) + " дней";
+    if (val === '') {
+        document.getElementById("result").innerHTML = "Вы ничего не ввели. Попробуйте еще раз";
+    } else {
+        if (isNaN(result) == true) {
+            document.getElementById("result").innerHTML = "Дата введена неверно. Попробуйте еще раз";
+        }  else {
+            document.getElementById("result").innerHTML = "До дня рождения осталось: " + Math.round(result) + " дней";
+        }
+    }
+
 }
