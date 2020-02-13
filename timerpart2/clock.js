8421a0e41ef33aec49c90ccc7bd41d996507e919
alekon94 <58603445+alekon94@users.noreply.github.com>
2020-02-13 16:39:32 +0300

    1
    1

--- timerpart2/clock.js
+++ timerpart2/clock.js
@@ -1,4 +1,26 @@
-function hi() {
-    alert ('hi');
+import { htmlElements } from "./tabs.js";
+
+
+function clockStart(id) {
+    id = setInterval(onIntervalNextTick, 1);
+
 }
-export {hi};
+
+function onIntervalNextTick() {
+    const clock = htmlElements.output;
+    const date = new Date(); // (*)
+
+    let hours = date.getHours();
+    if (hours < 10) hours = `0${hours}`;
+
+
+    let minutes = date.getMinutes();
+    if (minutes < 10) minutes = `0${minutes}`;
+
+
+    let seconds = date.getSeconds();
+    if (seconds < 10) seconds = `0${seconds}`;
+    clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+}
+export { clockStart, onIntervalNextTick };
